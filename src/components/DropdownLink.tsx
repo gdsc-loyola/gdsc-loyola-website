505f708d676acb551664052e17d11eab45f0c224
Paolo Tiu <paolotiu17@gmail.com>
2022-08-23 11:58:35 +0800

    Responsive navbar

--- src/components/DropdownLink.tsx
+++ src/components/DropdownLink.tsx
@@ -4,6 +4,14 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@radix-ui/react-dropdown-menu';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipPortal,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@radix-ui/react-tooltip';
+import { useState } from 'react';
 
 type Props = {
   label: string;
@@ -11,43 +19,91 @@ type Props = {
     label: string;
     href: string;
   }>;
+  fullWidth?: boolean;
+  inFlow?: boolean;
 };
 
-const DropdownLink = ({ items, label }: Props) => {
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger className="text-gray-500 hover:text-blue-400 font-medium transition-colors cursor-pointer flex gap-2">
-        {label}
-        <svg
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          className="fill-current"
-          xmlns="http://www.w3.org/2000/svg"
+export default function DropdownLink({ items, label, fullWidth, inFlow }: Props) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  if (inFlow) {
+    return (
+      <div>
+        <button
+          className={`text-gray-500 hover:text-blue-400 font-medium transition-colors cursor-pointer flex gap-2`}
+          onClick={() => setIsOpen((p) => !p)}
         >
-          <path d="M16.59 8.58984L12 13.1698L7.41 8.58984L6 9.99984L12 15.9998L18 9.99984L16.59 8.58984Z" />
-        </svg>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent
-        align="start"
-        sideOffset={5}
-        className="flex flex-col bg-white backdrop-filter "
-      >
-        {items.map(({ href, label }, i) => {
-          return (
-            <DropdownMenuItem asChild key={label + i}>
-              <a
-                href={href}
-                className="text-gray-500 hover:text-blue-400 font-medium transition-colors cursor-pointer flex gap-2 px-2 py-2 outline-none focus-visible:outline"
-              >
-                {label}
-              </a>
-            </DropdownMenuItem>
-          );
-        })}
-      </DropdownMenuContent>
-    </DropdownMenu>
-  );
-};
+          {label}
 
-export default DropdownLink;
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            className="fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M16.59 8.58984L12 13.1698L7.41 8.58984L6 9.99984L12 15.9998L18 9.99984L16.59 8.58984Z" />
+          </svg>
+        </button>
+
+        {isOpen && (
+          <div className="flex flex-col gap-5 pt-3">
+            {items.map(({ href, label }, i) => {
+              return (
+                <a
+                  href={href}
+                  key={i}
+                  className="text-gray-500 hover:text-blue-400 font-medium transition-colors cursor-pointer flex gap-2 px-2 py-2"
+                >
+                  {label}
+                </a>
+              );
+            })}
+          </div>
+        )}
+      </div>
+    );
+  }
+
+  return (
+    <TooltipProvider delayDuration={0}>
+      <Tooltip>
+        <TooltipTrigger
+          className={`text-gray-500 hover:text-blue-400 font-medium transition-colors cursor-pointer flex gap-2 ${
+            fullWidth && 'w-full justify-center py-2'
+          }`}
+        >
+          {label}
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            className="fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M16.59 8.58984L12 13.1698L7.41 8.58984L6 9.99984L12 15.9998L18 9.99984L16.59 8.58984Z" />
+          </svg>
+        </TooltipTrigger>
+        <TooltipPortal>
+          <TooltipContent
+            align="start"
+            sideOffset={5}
+            className="flex flex-col bg-white backdrop-filter "
+          >
+            {items.map(({ href, label }, i) => {
+              return (
+                <a
+                  key={i}
+                  href={href}
+                  className="text-gray-500 hover:text-blue-400 font-medium transition-colors cursor-pointer flex gap-2 px-2 py-2"
+                >
+                  {label}
+                </a>
+              );
+            })}
+          </TooltipContent>
+        </TooltipPortal>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
