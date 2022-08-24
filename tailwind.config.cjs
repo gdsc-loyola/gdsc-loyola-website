b5af69f3ccbb947f5f64b85dd6625b7d9d467bf3
Paolo Tiu <paolotiu17@gmail.com>
2022-08-24 12:07:39 +0800

    About section landing page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,16 @@ module.exports = {
         sans: ["Google Sans Display", "sans-serif"],
       },
       maxWidth: {
-        laptop: "1440px",
+        laptop: "1152px",
+      },
+      fontSize: {
+        xs: ["0.75rem", "1rem"],
+        sm: ["0.875rem", "1.5rem"],
+        base: ["1rem", "1.5rem"],
+        md: ["1.25rem", "2rem"],
+        lg: ["1.5rem", "2.5rem"],
+        xl: ["2rem", "3rem"],
+        "2xl": ["2.5rem", "3.5rem"],
       },
       colors: {
         white: "#FFFFFF",
