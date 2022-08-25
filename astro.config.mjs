2f2264c1ee0417a587ac368367e6b2daad470160
Paolo Tiu <paolotiu17@gmail.com>
2022-08-25 11:53:57 +0800

    Add astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import image from "@astrojs/image";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), image()],
+  integrations: [tailwind(), react(), image(), sitemap()]
 });
