3d3f6b012fe33988b5f10ecfe9c289ab03c9f5a7
Paolo Tiu <paolotiu17@gmail.com>
2022-09-01 17:22:45 +0800

    better brands image handling

---
+++ scripts/updateBrands.js
@@ -0,0 +1,35 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+function upperFirst(string) {
+  return string.slice(0, 1).toUpperCase() + string.slice(1, string.length);
+}
+function snakeToPascal(string) {
+  return string
+    .split("_")
+    .map((str) => {
+      return upperFirst(str.split("/").map(upperFirst).join("/"));
+    })
+    .join("");
+}
+
+async function run() {
+  const dir = (
+    await fs.readdir(
+      path.join(__dirname, "../src/components/Landing/assets/brands")
+    )
+  ).filter((file) => file.endsWith(".png"));
+
+  const imports = dir.map(
+    (brand) =>
+      `import ${brand.split(".")[0].replaceAll("-", "_")} from './${brand}'`
+  );
+  const exportDefault = `export default [${dir
+    .map((brand) => brand.split(".")[0].replaceAll("-", "_"))
+    .join(",")}];`;
+  fs.writeFile(
+    path.join(__dirname, "../src/components/Landing/assets/brands/brands.js"),
+    `${imports.join("\n")}\n\n${exportDefault}`
+  );
+}
+run();
---
+++ src/components/Landing/assets/brands/brands.js
@@ -0,0 +1,12 @@
+import accenture from './accenture.png'
+import anc from './anc.png'
+import datacamp from './datacamp.png'
+import digital_ocean from './digital-ocean.png'
+import inquirer_net from './inquirer-net.png'
+import nextpay from './nextpay.png'
+import paymongo from './paymongo.png'
+import replit from './replit.png'
+import ux_ph from './ux-ph.png'
+import when_in_manila from './when-in-manila.png'
+
+export default [accenture,anc,datacamp,digital_ocean,inquirer_net,nextpay,paymongo,replit,ux_ph,when_in_manila];
